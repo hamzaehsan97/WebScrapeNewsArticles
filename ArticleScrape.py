f7105d7b0da5b172bbaf96ffb3ab24d3ee55afae
Hamza Ehsan <37129498+hamzaehsan97@users.noreply.github.com>
2019-07-08 14:32:03 -0500

    fixed a typo

--- ArticleScrape.py
+++ ArticleScrape.py
@@ -1,15 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
-companyName = input('Enter Company name:')
-page = requests.get('https://news.google.com/search?q='+companyName+'%20when%3A1y&hl=en-US&gl=US&ceid=US%3Aen')
-soup = BeautifulSoup(page.content, 'html.parser')
+keyword = input('Enter keyword:')
 
-weblinks = soup.find_all('article')
-pagelinks = []
-for link in weblinks[1:10]:
-      urlLink = link
-      url = link.contents[0].get('href')
-      title = link.contents[1].find('a').getText()
-      infoTuple = (('http://news.google.com'+url),title)
-      pagelinks.append(infoTuple)
-print(pagelinks)
+def returnArticles(keyword):
+    page = requests.get('https://news.google.com/search?q='+keyword+'%20when%3A1y&hl=en-US&gl=US&ceid=US%3Aen')
+    soup = BeautifulSoup(page.content, 'html.parser')
+    weblinks = soup.find_all('article')
+    pagelinks = []
+    for link in weblinks[1:10]:
+          url = link.contents[0].get('href')
+          title = link.contents[1].find('a').getText()
+          infoTuple = (('http://news.google.com'+url),title)
+          pagelinks.append(infoTuple)
+          return pagelinks
+
+print(returnArticles(keyword))
